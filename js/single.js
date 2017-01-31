de3dad454870c67a6b2c61fab014a499113bb642
井本拓伸 <inmthrnb@gmail.com>
2017-01-31 22:55:33 +0900

    indexページに機能を集約する

--- js/single.js
+++ js/single.js
@@ -3,15 +3,31 @@ var app = new Vue({
     data: {
         message: "message",
         seen: true,
-        stored: ""
+        stored: localStorage.getItem("stored"),
+        html: ""
     },
     methods: {
         done: function(event) {
             console.log(event);
         },
         store: function(event) {
-        	console.log("store");
-            localStorage.setItem("store", this.store);
+        	console.log("stored");
+            localStorage.setItem("stored", this.stored);
+        },
+        load: function() {
+            jQuery.ajax({
+                type: "GET",
+                dataType: "xml",
+                url: this.html,
+                success: function(data) {
+                    var v = jQuery(data.documentElement).find("meta");
+                    console.log(v);
+                },
+                error: function(error,e) {
+                    console.log(e);
+                    console.warn(error);
+                }
+            });
         }
     }
 });
