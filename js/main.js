bc0027166fd5f84feece21b7d899e49c892a7838
井本拓伸 <inmthrnb@gmail.com>
2017-02-01 16:08:09 +0900

    細かい修正

--- js/main.js
+++ js/main.js
@@ -11,82 +11,81 @@ function displaySample() {
 	var displaySampleElem = document.getElementById("display-sample");
 	displaySampleElem.appendChild(document.createTextNode(sample.firstChild.nodeValue));
 }
-
-function loadSvg(target, url) {
+function createImage(target, src) {
+	$(target).append('<img src="' + location.href + src + '"' + $(target).textContent+'">');
+}
+function loadXml(target, url) {
+	console.log(target);
 	$.ajax({
 		type: "GET",
 		dataType: "xml",
 		url: url,
-		success: function(data) {
-			console.log(data);
+		success: function (data) {
 			console.log(data.documentElement);
 			$(target).empty().append($(data.documentElement));
 		}
 	});
 }
 
-var hcjapp = new Vue({
-    el:"#hcjapp",
-    data: {
-        
-    },
-    methods: {
-		load: function(event) {
-			console.log(event);
-			if(event.srcElement.nodeName === 'CODE') {
-				console.log(event.srcElement.textContent);
-				jQuery.ajax({
-					type:"GET",
-					dataType:"xml",
-					url: event.srcElement.textContent,
-					success: function(data) {
-						
-						jQuery("#document").empty().append($(data).find("main > *"));
-						console.log();
-					},
-					error: function() {
 
-					}
-				});
-			}
-		}
-    }
-});
 
 var application;
-Vue.component("test",{
+Vue.component("test", {
 	template: "<li>test</test>"
 });
-Vue.component("todo-item",{
+Vue.component("todo-item", {
 	props: ['todo'],
 	template: "<li>{{todo.text}}</li>"
 });
 
-window.addEventListener("load",function() {
-	jQuery("#contents code").each(function(i,code) {
-		console.log(i);
-		console.log(code);
-		console.log(code.parentNode);
+window.addEventListener("load", function () {
+	
+	var hcjapp = new Vue({
+		el: "#hcjapp",
+		data: {
+
+		},
+		methods: {
+			load: function (event) {
+				console.log(event);
+				if (event.srcElement.nodeName === 'CODE') {
+					console.log(event.srcElement.textContent);
+					jQuery.ajax({
+						type: "GET",
+						dataType: "xml",
+						url: event.srcElement.textContent,
+						success: function (data) {
+							jQuery("#document").empty().append($(data).find("main > *"));
+							jQuery("#sidebox").empty().append($(data).find("aside > *"));
+						},
+						error: function (data) {
+							console.warn(data);
+						}
+					});
+				}
+			}
+		}
+	});
+	jQuery("#contents code").each(function (i, code) {
 		jQuery.ajax({
 			type: "GET",
 			dataType: "xml",
 			url: code.textContent,
-			success: function(data) {
+			success: function (data) {
 				var meta = {
 					title: null,
 					description: null,
 				};
-				$(data.documentElement).find("meta[property]").each(function(i,d) {
-					console.log(i);
-					console.log(d);
-
-					switch(d.getAttribute("property")) {
+				$(data.documentElement).find("meta[property]").each(function (i, d) {
+					switch (d.getAttribute("property")) {
 						case "og:title":
-						meta.title = d.getAttribute("content");
-						break;
+							meta.title = d.getAttribute("content");
+							break;
+						case "og:description":
+							meta.description = d.getAttribute("content");
 					}
 				});
-				jQuery(code.parentNode).before('<span>' + meta.title + '</span>',code);
+				jQuery(code.parentNode).before('<span>' + meta.title + " : " + meta.description + '</span>', code);
 			}
 		});
 	});
@@ -99,25 +98,25 @@ function newApp() {
 			message: "message",
 			seen: true,
 			todos: [
-				{text:"vuejs"},
-				{text:"typescript"},
-				{text:"cssframework"}
+				{ text: "vuejs" },
+				{ text: "typescript" },
+				{ text: "cssframework" }
 			],
 			stored: "",
 			model: "model"
 		},
 		methods: {
-			log: function(e) {
+			log: function (e) {
 				console.log(e);
 			},
-			done: function(event) {
-            	console.log(event);
-        	},
-        	store: function(event) {
-        		console.log("store");
-            	localStorage.setItem("stored", this.stored);
-        	},
-			reverseMessage: function() {
+			done: function (event) {
+				console.log(event);
+			},
+			store: function (event) {
+				console.log("store");
+				localStorage.setItem("stored", this.stored);
+			},
+			reverseMessage: function () {
 				console.log(e);
 				this.message = this.message.split("").reverse().join("");
 			}
