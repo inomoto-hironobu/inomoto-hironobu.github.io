d595b2a2b0edeff49e940917bc1a2bace364b023
井本拓伸 <inmthrnb@gmail.com>
2020-05-10 19:09:24 +0900

    サイトの体裁を整え充実

--- js/controller.js
+++ js/controller.js
@@ -1,22 +1,47 @@
 window.addEventListener('DOMContentLoaded', function() {
-	$('#accordion > article')
-	.filter(function() {
-		return checkid(this);
-	})
-	.each(function() {
-		if($('#アイデア').has(this)) {
-			var tabpanel = $(this).find('[role=\'tabpanel\']');
-			tabpanel.addClass('show');
-			console.log(tabpanel);
-			//console.log(tabpanel.attr('class'));
-			$('#mainlist > .active').toggleClass('active');
-			$('#アイデア-tab').toggleClass('active');
-			$('#ホーム').toggleClass('active').toggleClass('show');
-			$('#アイデア').toggleClass('active').toggleClass('show');
-		}
-	});
+	
 });
-
+/**
+ * 
+ * @param {Node} target 
+ * @param {*} value 
+ */
+function arialabel(target) {
+	function exec(tabpanel) {
+		var tab = document.getElementById(tabpanel.getAttribute('aria-labelledby'));
+		applyDescendants(
+			tabpanel.parentNode
+			, function(node, v) {
+				console.log(v.i + node);
+				return v.i === 0 && node.nodeType === Node.ELEMENT_NODE && node.getAttribute('role') === 'tabpanel';
+			}, function(tpchild, v) {
+				console.log(v.i++ + tpchild);
+				var jquery = $(tpchild);
+				if(jquery.hasClass('active') || tabpanel === tpchild) {
+					jquery.toggleClass('active');
+				}
+				if(jquery.hasClass('show') || tabpanel === tpchild) {
+					jquery.toggleClass('show');
+				}
+				return v;
+			}, {i:0}
+		);
+		return tab;
+	}
+	if(target.getAttribute('role') === 'tabpanel') {
+		return exec(target, 0);
+	} else {
+		return applyAncestor(
+			target
+			, function(node) {
+				return node.getAttribute('role') === 'tabpanel';
+			}, function(tabpanel) {
+				return exec(tabpanel);
+			},
+			null);
+	}
+    
+}
 function checkid(e) {
 	return decodeURI(location.hash.substring(1)) == $(e).attr('id');
 }
