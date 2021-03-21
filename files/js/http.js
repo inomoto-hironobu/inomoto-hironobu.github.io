89ffbfad0805e5bd3cd546a919f63f9c91b94539
井本拓伸 <inmthrnb@gmail.com>
2021-03-21 18:21:34 +0900

    不要ファイルの削除

--- files/js/http.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', function(e) {
-	var m = new Date(document.lastModified);
-	var 年月日 = m.getFullYear() + "年" + (m.getMonth() + 1) + "月" + (m.getDate()) + "日"; 
-	document
-		.getElementById('modified')
-		.appendChild(document.createTextNode(年月日));
-});
