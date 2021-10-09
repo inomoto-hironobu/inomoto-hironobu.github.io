cbdb889b46240db1ec15324f83507f05d6c047fb
井本拓伸 <inmthrnb@gmail.com>
2021-10-09 18:57:43 +0900

    反カルトビジネスを思いついた
    リンクの追加とついでにサイドバーの挙動の調整

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 window.addEventListener('DOMContentLoaded', ()=>{
+	console.log(window);
+	if(window.innerWidth > 750) {
+		
+		document
+		.querySelectorAll('aside details')
+		.forEach((details)=>{
+			details.setAttribute('open', '');
+		});
+	}
 	document
-	.querySelectorAll('#osusume li')
+	.querySelectorAll('.article-list li')
 	.forEach((li)=>{
 		const p = document.createElement('p');
 		li.appendChild(p);
 		pullMeta(p);
 	});
+
 });
