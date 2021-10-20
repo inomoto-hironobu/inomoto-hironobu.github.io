5669044f17c176188288a2cb77ad27745d7c6cfc
井本拓伸 <inmthrnb@gmail.com>
2021-10-21 00:12:05 +0900

    変更で表示がおかしかったところがあったのでサイトの構成ファイルを編集した

--- js/main.js
+++ js/main.js
@@ -15,5 +15,15 @@ window.addEventListener('DOMContentLoaded', ()=>{
 		li.appendChild(p);
 		pullMeta(p);
 	});
-
+	const githubHistoryLink = document
+	.createElement('a');
+	githubHistoryLink.setAttribute('href','https://github.com/inomoto-hironobu/inomoto-hironobu.github.io/commits/master/'+window.location.pathname);
+	githubHistoryLink.setAttribute('target','_blank');
+	githubHistoryLink.appendChild(document.createTextNode('ファイルの変更履歴'));
+	document
+	.getElementById('github-history')
+	.appendChild(githubHistoryLink);
+	
 });
+
+
