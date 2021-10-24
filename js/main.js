4dd3ec54f905c23fc91ef793be22eabe2ffccb69
井本拓伸 <inmthrnb@gmail.com>
2021-10-24 20:32:54 +0900

    リンク先読みプログラムを実行するページは単独記事系のページだけで動作させる

--- js/main.js
+++ js/main.js
@@ -25,23 +25,6 @@ window.addEventListener('DOMContentLoaded', ()=>{
 	document
 	.getElementById('github-history')
 	.appendChild(githubHistoryLink);
-	
-	document
-	.querySelectorAll('main a[href]')
-	.forEach((a)=>{
-		console.log(a.getAttribute('href'));
-		const href = new URL(a.getAttribute('href'),window.location);
-		const windoworigin = new URL(window.location).origin;
-		if(href.origin === windoworigin) {
-			const button = document.createElement('button');
-			button.appendChild(document.createTextNode('情報を見る'));
-			button.onclick = function() {
-				pullMetaAfter(button,a);
-				button.style.display = 'none';
-			};
-			a.parentNode.insertBefore(button, a.nextSibling);
-		}		
-	});
 });
 
 
