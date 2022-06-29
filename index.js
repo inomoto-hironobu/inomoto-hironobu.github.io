d809745edd444ae3596b099b5220613b7b266528
井本拓伸 <inmthrnb@gmail.com>
2022-06-29 09:08:06 +0900

    特別に用意したコードを削除

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ app.listen(8080, () => {
 });
 
 // 静的ファイルのルーティング
-//Framexsテーマkumoriを使う。docsへのルーティングが必要
-app.use(`/kumori`, express.static(path.join(__dirname, 'kumori/docs')));
 app.use(express.static(path.join(__dirname, '')));
 
 // その他のリクエストに対する404エラー
