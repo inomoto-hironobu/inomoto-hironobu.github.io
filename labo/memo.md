5850f1c3535dcf05e025d97b6290d2589b6d7b7e
井本拓伸 <inmthrnb@gmail.com>
2022-12-12 17:37:36 +0900

    実験的コードの追加

---
+++ labo/memo.md
@@ -0,0 +1,3 @@
+
+https://www.saxonica.com/saxon-js/documentation2/index.html#!api/xpathEvaluate
+If a JavaScript Array is supplied as the context item, it is assumed to represent an XDM sequence, according to the JavaScript to XDM convention. If the array is empty, the context item is absent. If the array has more than one member, a cardinality error is thrown.
---
+++ labo/testxpath.js
@@ -0,0 +1,94 @@
+function test1(){
+    let array = [1,"2",[3]];
+
+    let obj = {
+        test:array
+    };
+
+    let xpath = "array:filter(.,function($a){$a='2'})";
+
+    //
+    let result = SaxonJS.XPath.evaluate(xpath,array);
+
+    console.log(result);
+}
+
+
+
+function test1(){
+let array = [1,"2",[3]];
+
+let obj = {
+    test:array
+};
+
+let xpath = "array:filter(function($a){$a='2'})";
+
+//
+let result = SaxonJS.XPath.evaluate(xpath,array);
+
+console.log(result);
+}
+
+try {
+    test1();
+} catch(e){
+    console.error(e);
+}
+
+
+function test2(){
+    let array = [1];
+    
+    let xpath = ".=1";
+    //
+    let result = SaxonJS.XPath.evaluate(xpath,array);
+    
+    console.log(result);
+}
+try {
+    test2();
+} catch(e){
+    console.error(e);
+}
+
+function test3(){
+let array = [[1,2]];
+
+let obj = {
+    test:array
+};
+
+let xpath = ".[1]=>array:filter(function($a){$a=2})";
+
+//
+let result = SaxonJS.XPath.evaluate(xpath,array);
+
+console.log(result);
+}
+
+try {
+    test3();
+} catch(e){
+    console.error(e);
+}
+
+function test4(){
+
+let obj = {
+    test:[[1],[2]]
+};
+
+let xpath = ".?test=>tail()=>array:get(1)";
+
+//
+let result = SaxonJS.XPath.evaluate(xpath,obj);
+
+console.log(result);
+}
+
+try {
+    test4();
+} catch(e){
+    console.error(e);
+}
