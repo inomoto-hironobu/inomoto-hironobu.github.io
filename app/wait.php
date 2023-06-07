2e5463c5e3150593a6c12e69c12b890f5be64d45
井本拓伸 <inmthrnb@gmail.com>
2023-06-07 16:54:28 +0900

    change

--- app/wait.php
+++ app/wait.php
@@ -1,6 +1,7 @@
 <?php
 if(isset($_GET['time'])) {
     echo 'wait'.$_GET['time'].'...';
-    sleep((int) $_GET['time']);
+    $sleep = sleep((int) $_GET['time']);
+    echo $sleep.'...';
 }
 echo 'end';
